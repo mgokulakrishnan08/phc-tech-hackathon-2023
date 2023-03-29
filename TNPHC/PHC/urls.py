74a2f9f61f9f2a6c2ac6ba7797080e183d35de83
mgokulakrishnan31 <92560183+mgokulakrishnan31@users.noreply.github.com>
2023-03-29 08:29:12 +0530

    home page created and navigated from login

--- TNPHC/PHC/urls.py
+++ TNPHC/PHC/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('',login)
+    path('',login),
+    path('<str:code>/',home)
 ]
--- TNPHC/PHC/views.py
+++ TNPHC/PHC/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def login(request):
-    return HttpResponse('hello world')
+    if request.GET:
+        phc_id=request.GET.dict()['phc_id']
+        password=request.GET.dict()['password']
+        if password==PHC.objects.get(phc_id=phc_id).password:
+            return redirect(f'{phc_id}/') 
+            
+
+    return render(request, 'PHC/login.html')
+
+
+
+def home(request,code):
+    x=PHC.objects.get(phc_id=code)
+
+    return render(request, 'PHC/home.html',{'x':x})
+   
